fab4e0662fe1d6fb2bdccfe75f1bbc3f8f853a3d
Master_shiv.info.me <103109048+shivamkumarsha@users.noreply.github.com>
2024-06-01 16:43:58 +0530

    final product

--- app.js
+++ app.js
@@ -2,20 +2,29 @@ import express from "express";
 import cors from "cors";
 import connectDb from "./utils/db.js";
 import bodyParser from "body-parser";
+import router from "./routers/auth-router.js";
+
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const allowedOrigins = [
+  `${process.env.FRONTENDLINK}`,
+  `${process.env.FRONTENDLINK2}`,
+];
+
 const corsOptions = {
-  origin: `${process.env.FRONTENDLINK}`,
+  origin: allowedOrigins,
   methods: "GET,POST,PUT,DELETE,PATCH,HEAD,OPTIONS",
   credentials: true,
 };
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(cors(corsOptions));
-import router from "./routers/auth-router.js";
-const port = process.env.PORT || 4000;
 app.use("/api/", router);
+
+const port = process.env.PORT || 4000;
 
 connectDb().then(() => {
   app.listen(port, () => {
